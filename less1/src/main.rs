07d8ab4eb9673f6969041ac494d3da4c32e13458
Rahul Anand <bliss@Rahuls-MacBook-Pro.local>
2025-11-30 16:37:09 +0530

    setup & guess game add in less1

--- less1/src/main.rs
+++ less1/src/main.rs
@@ -1,3 +1,17 @@
+use std::io;
+
 fn main() {
     println!("Hello, world!");
+
+    // Gueesing game
+
+    println!("Guessing Game---");
+
+    let mut guess=String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("failed to read line");
+    
+    println!("You guessed: {guess}");
 }
