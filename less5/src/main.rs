4372f39ec3ef0bb0355013dfbb45bde773f109ef
Rahul Anand <bliss@Rahuls-MacBook-Pro.local>
2025-12-07 10:44:54 +0530

    web server fmt

--- less5/src/main.rs
+++ less5/src/main.rs
@@ -1,35 +1,40 @@
-use axum::{
-    routing::get,
-    Router,
-    response::IntoResponse,
-};
-use axum::extract::{Path, Query, Json};
+use axum::extract::{Json, Path, Query};
+use axum::{Router, response::IntoResponse, routing::get};
 use serde_json::{Value, json};
-
 
 #[tokio::main]
 async fn main() {
     let app = Router::new()
-    .route("/", get(root))
-    .route("/foo", get(get_foo))
-    .route("/foo/bar", get(foo_bar))
-    .route("/health", get(health_check));
+        .route("/", get(root))
+        .route("/foo", get(get_foo))
+        .route("/foo/bar", get(foo_bar))
+        .route("/health", get(health_check))
+        .route("/user/{id}", get(get_user));
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
-
 }
 
-async fn root()-> &'static str {
+async fn root() -> &'static str {
     "Do something else"
 }
-async fn get_foo()-> &'static str {
+async fn get_foo() -> &'static str {
     "Getting Foo"
 }
-async fn foo_bar()-> &'static str {
-    "Getting Foobar"
+async fn foo_bar() -> &'static str {
+    "Getting Foobar checking if things are working"
 }
 
-async fn health_check()->  Json<Value>{
+async fn get_user(Path(id): Path<u32>) -> Json<Value> {
+    Json(json!({
+        "status":"200",
+        "message":"Received successfully",
+        "data":{
+            "userId":id
+        }
+    }))
+}
+
+async fn health_check() -> Json<Value> {
     Json(json!({
         "status":"ok",
         "message":"Server is running"
